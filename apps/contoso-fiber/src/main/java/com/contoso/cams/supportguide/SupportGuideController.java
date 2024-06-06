4fc4b8ce0f92500dec50fc76bc9f5cafe263374b
Nick Dalalelis <ndalalelis@gmail.com>
2024-06-06 09:24:07 -0400

    Upload to blob storage

--- apps/contoso-fiber/src/main/java/com/contoso/cams/supportguide/SupportGuideController.java
+++ apps/contoso-fiber/src/main/java/com/contoso/cams/supportguide/SupportGuideController.java
@@ -1,5 +1,6 @@
 package com.contoso.cams.supportguide;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -7,8 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @AllArgsConstructor
@@ -22,5 +27,19 @@ public class SupportGuideController {
         List<SupportGuideDto> serviceGuides = guideService.getSupportGuides();
         model.addAttribute("guides", serviceGuides);
         return "pages/guides/list";
+    }
+
+    @GetMapping("/upload")
+    @PreAuthorize("hasAnyAuthority('APPROLE_AccountManager')")
+    public String displayUploadGuideForm() {
+        return "pages/guides/upload";
+    }
+
+    @PostMapping("/upload")
+    public String uploadGuide(@RequestParam("guide") MultipartFile file) throws IOException {
+
+        guideService.uploadGuide(file.getOriginalFilename(), file);
+
+        return "redirect:/guides/list";
     }
 }
--- apps/contoso-fiber/src/main/java/com/contoso/cams/supportguide/SupportGuideService.java
+++ apps/contoso-fiber/src/main/java/com/contoso/cams/supportguide/SupportGuideService.java
@@ -1,19 +1,36 @@
 package com.contoso.cams.supportguide;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
 import com.contoso.cams.model.SupportGuideRepository;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class SupportGuideService {
 
+    private static final String BLOB_RESOURCE_PATTERN = "azure-blob://%s/%s";
+
     private final SupportGuideRepository guideRepository;
+    private final ResourceLoader resourceLoader;
+
+    @Value("${spring.cloud.azure.storage.blob.container-name}")
+    private final String blobContainerName;
 
     public List<SupportGuideDto> getSupportGuides() {
         return guideRepository.findAll().stream()
@@ -23,5 +40,18 @@ public class SupportGuideService {
                 guide.getDescription(),
                 guide.getUrl()))
             .collect(Collectors.toList());
+    }
+
+    public void uploadGuide(String fileName, InputStreamSource guideInputStream) throws IOException {
+        var location = String.format(BLOB_RESOURCE_PATTERN, blobContainerName, fileName);
+        log.info("Saving guide: {} to {}", fileName, location);
+
+        Resource resource = resourceLoader.getResource(location);
+        try (OutputStream outputStream = ((WritableResource) resource).getOutputStream();
+             InputStream inputStream = guideInputStream.getInputStream()) {
+            FileCopyUtils.copy(inputStream, outputStream);
+        }
+
+        //guideRepository.save(guide.toEntity());
     }
 }
