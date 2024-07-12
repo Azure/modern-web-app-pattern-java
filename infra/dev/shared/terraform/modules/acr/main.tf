3913f4711aa39a30191bdabdbdf09a4a0fbfd93a
Isabelle Bersano <ibersano@microsoft.com>
2024-07-12 16:51:35 -0400

    feat: initial acr module

---
+++ infra/dev/shared/terraform/modules/acr/main.tf
@@ -0,0 +1,105 @@
+terraform {
+  required_providers {
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "1.2.26"
+    }
+  }
+}
+
+data "azuread_client_config" "current" {}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.application_name
+  resource_group_name = var.resource_group
+  location            = var.location
+
+  sku = "Premium"
+
+  admin_enabled                 = false
+  public_network_access_enabled = var.environment == "prod" ? false : true
+  network_rule_bypass_option    = "AzureServices"
+
+  dynamic "georeplications" {
+    for_each = var.georeplications
+    content {
+      location = georeplications.value.location
+    }
+    
+  }
+  network_rule_set {
+    default_action = var.network_rules.default_action
+
+    dynamic "ip_rule" {
+      for_each = var.network_rules.ip_rules != null ? var.network_rules.ip_rules : []
+      content {
+        action   = ip_rule.value.action
+        ip_range = ip_rule.value.ip_range
+      }
+    }
+  }
+}
+
+resource "azurerm_role_assignment" "container_app_acr_pull" {
+  principal_id                     = var.aca_identity_principal_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+}
+
+resource "azurerm_user_assigned_identity" "container_registry_user_assigned_identity" {
+  name                = "ContainerRegistryUserAssignedIdentity"
+  resource_group_name = var.resource_group
+  location            = var.location
+}
+
+resource "azurerm_role_assignment" "container_registry_user_assigned_identity_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.container_registry_user_assigned_identity.principal_id
+}
+
+
+# For demo purposes, allow current user access to the container registry
+# Note: when running as a service principal, this is also needed
+resource azurerm_role_assignment acr_contributor_user_role_assignement {
+  scope                 = azurerm_container_registry.acr.id
+  role_definition_name  = "Contributor"
+  principal_id          = data.azuread_client_config.current.object_id
+}
+
+# Azure Private DNS provides a reliable, secure DNS service to manage and
+# resolve domain names in a virtual network without the need to add a custom DNS solution
+# https://docs.microsoft.com/azure/dns/private-dns-privatednszone
+resource "azurerm_private_dns_zone" "dns_for_acr" {
+  count               = var.environment == "prod" ? 1 : 0
+  name                = "privatelink.azurecr.io"
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "virtual_network_link_acr" {
+  count                 = var.environment == "prod" ? 1 : 0
+  name                  = "privatelink.azurecr.io"
+  private_dns_zone_name = azurerm_private_dns_zone.dns_for_acr[0].name
+  virtual_network_id    = var.private_endpoint_vnet_id
+  resource_group_name   = var.resource_group
+}
+
+resource "azurerm_private_endpoint" "acr_pe" {
+  count               = var.environment == "prod" ? 1 : 0
+  name                = "private-endpoint-acr"
+  location            = var.location
+  resource_group_name = var.resource_group
+  subnet_id           = var.private_endpoint_subnet_id
+
+   private_dns_zone_group {
+    name                 = "privatednsacrzonegroup"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns_for_acr[0].id]
+  }
+
+  private_service_connection {
+    name                           = "peconnection-acr"
+    private_connection_resource_id = azurerm_container_registry.acr.id
+    is_manual_connection           = false
+    subresource_names              = ["registry"]
+  }
+}
---
+++ infra/dev/shared/terraform/modules/acr/variables.tf
@@ -0,0 +1,57 @@
+variable "resource_group" {
+  type        = string
+  description = "The resource group"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment (dev, test, prod...)"
+  default     = "dev"
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where all resources in this example should be created"
+}
+
+variable "application_name" {
+  type        = string
+  description = "The name of your application"
+}
+
+variable "aca_identity_principal_id" {
+  type        = string
+  description = "The principal id of the identity of the container app"
+}
+
+variable "network_rules" {
+  type = object({
+    default_action = string
+    ip_rules = list(object({
+      action   = string
+      ip_range = string
+    }))
+  })
+
+  default = {
+    default_action = "Allow"
+    ip_rules = []
+  }
+}
+
+variable "georeplications" {
+  type = list(object({
+    location = string
+  })) 
+  default = []
+}
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "The ID of the subnet where the private endpoint should be created"
+}
+
+variable "private_endpoint_vnet_id" {
+  type        = string
+  description = "The ID of the VNet where the private endpoint should be created"
+}
