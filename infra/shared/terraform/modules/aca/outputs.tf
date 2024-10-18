78e597517e5ed0c76d32a21bfc3712b073c3f4ed
Nick Dalalelis <ndalalelis@microsoft.com>
2024-10-18 15:59:18 +0000

    fix Terraform ACA output for dev deployments

--- infra/shared/terraform/modules/aca/outputs.tf
+++ infra/shared/terraform/modules/aca/outputs.tf
@@ -3,9 +3,9 @@ output "identity_principal_id" {
 }
 
 output "default_domain" {
-  value = azurerm_container_app_environment.container_app_environment_prod[0].default_domain
+  value = length(azurerm_container_app_environment.container_app_environment_prod) > 0 ? azurerm_container_app_environment.container_app_environment_prod[0].default_domain : ""
 }
 
 output "static_ip_address" {
-  value = azurerm_container_app_environment.container_app_environment_prod[0].static_ip_address
+  value = length(azurerm_container_app_environment.container_app_environment_prod) > 0 ? azurerm_container_app_environment.container_app_environment_prod[0].static_ip_address : ""
 }
